b437ada319322710b259aa8013bc481b869f867c
DinuEugen <dinueugenmarin@gmail.com>
2024-10-23 10:54:07 +0200

    First commit HomeController

---
+++ resources/views/about.blade.php
@@ -0,0 +1,2 @@
+<h1>Hello laravel</h1>
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias corrupti distinctio magnam ullam veniam itaque? Corrupti molestiae libero ullam dolorem.</p>
---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+
+
+
+
+Route::view('/about','about');
+
+
+
+
+
+
+
